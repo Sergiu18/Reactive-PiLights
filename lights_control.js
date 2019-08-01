ae9897069a58a190ae4c103cf637ea38f46fb3e4
Sergiu Micorici <sergiumicorici@gmail.com>
2019-08-01 12:00:44 +0300

    added lights_control and index

---
+++ lights_control.js
@@ -0,0 +1,79 @@
+const GPIO = require('pigpio').Gpio;
+const red =  new GPIO(27, {mode: GPIO.OUTPUT});
+const green = new GPIO(17, {mode: GPIO.OUTPUT});
+const blue = new GPIO(22, {mode: GPIO.OUTPUT});
+var currentColor = {
+	red: 180,
+	green: 80,
+	blue: 50
+}
+
+var stroboscopLoop;
+
+function lights_off()
+{
+	red.pwmWrite(0);
+	green.pwmWrite(0);
+	blue.pwmWrite(0);
+}
+
+function set_color(r, g, b)
+{
+	red.pwmWrite(r);
+	green.pwmWrite(g);
+	blue.pwmWrite(b);
+
+	currentColor =  {
+		red: r,
+		green: g,
+		blue: b
+	}
+}
+
+function stroboscopic_on()
+{
+	var stroboscop = true;
+	stroboscopLoop = setInterval(function(){
+		console.log(stroboscop ? "lights on" : "lights off")
+		if(stroboscop){
+			set_color(currentColor.red, currentColor.green, currentColor.blue);
+		}
+		else {
+			lights_off();
+		}
+		stroboscop = !stroboscop;
+	}, 500);
+}
+
+function stroboscopic_off()
+{
+	clearInterval(stroboscopLoop);
+}
+
+function rainbow_on()
+{
+
+	for(g=255;g>=199;g=-2)
+		for(r=199;r<=255;r=+2)
+			for(b=255;b>=199;b=-2)	
+				for(g=199;g<=255;g=+2)
+					for(r=255;r>=199;r=-2)
+						for(b=199;b<=255;b=+2)
+							set_color(r,g,b)
+
+}
+
+function rainbow_off()
+{
+	//clearInterval(stroboscopLoop);
+}
+
+
+
+
+module.exports = { 
+	lights_off,
+	set_color,
+	stroboscopic_on,
+	stroboscopic_off
+}
