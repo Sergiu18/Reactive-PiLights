b57a8d22849b16ea8b775df83460c66c52449143
Sergiu Micorici <sergiumicorici@gmail.com>
2019-08-02 18:40:05 +0300

    half step for rainbow

--- lights_control.js
+++ lights_control.js
@@ -70,7 +70,7 @@ function rainbow_on()
 			   const green = Math.round(Math.sin(frequency*i + 2) * 127 + 128);
 			   const blue  = Math.round(Math.sin(frequency*i + 4) * 127 + 128);
 			   set_color(red, green, blue);
-		   }, 100*i);
+		   }, 50*i);
 		}
 	
 }
