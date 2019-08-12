0485f5f6e4e79d2a158a7dcb3557806059222ad4
Sergiu Micorici <sergiumicorici@gmail.com>
2019-08-12 13:16:18 +0300

    tweaked rainbow

--- lights_control.js
+++ lights_control.js
@@ -69,9 +69,9 @@ function rainbow_cycle()
 		   const red   = Math.round(Math.sin(frequency*i + 0) * 127 + 128);
 		   const green = Math.round(Math.sin(frequency*i + 2) * 127 + 128);
 		   const blue  = Math.round(Math.sin(frequency*i + 4) * 127 + 128);
-		   red.pwmWrite(r);
-			green.pwmWrite(g);
-			blue.pwmWrite(b);
+		   red.pwmWrite(red);
+			green.pwmWrite(green);
+			blue.pwmWrite(blue);
 	   }, 50*i);
 	}
 }
