b2df7041f7e88b86d4a9cf02a0269cce28ceb84c
Sergiu Micorici <sergiumicorici@gmail.com>
2019-08-12 12:05:54 +0300

    ceva

--- index.js
+++ index.js
@@ -52,8 +52,7 @@ app.get('/api/toggleStroboscopic', (req, res) => {
 });
 
 app.get('/api/toggleRainbow', (req, res) => {
-	lightController.rainbow_off();
-	if(lightController.state.rainbow)
+	if(lightController.state.rainbow==true)
 		res.send(lightController.rainbow_off());
 	else
 	{
--- lights_control.js
+++ lights_control.js
@@ -64,7 +64,7 @@ function rainbow_cycle()
 	var frequency = 0.063;
 	for (let i = 0; i < 100; ++i)
 	{
-		const timeout = setTimeout(() => {
+		setTimeout(() => {
 		   const red   = Math.round(Math.sin(frequency*i + 0) * 127 + 128);
 		   const green = Math.round(Math.sin(frequency*i + 2) * 127 + 128);
 		   const blue  = Math.round(Math.sin(frequency*i + 4) * 127 + 128);
