db7639f38fe3754a8d50341bb2a04d0ce01c81a3
Sergiu Micorici <sergiumicorici@gmail.com>
2019-09-02 16:48:45 +0300

    added state function

--- index.js
+++ index.js
@@ -4,92 +4,10 @@ const cors = require('cors');
 
 const app = express();
 
-app.use('/', express.static('public'));
-
-app.get('/api/lights_off', cors(), (req, res) => {
-	console.log("lights_off called")
-	lightController.stroboscopic_off();
-	lightController.rainbow_off();
-	lightController.lights_off();
-	res.send({message: "Lights turned off", color: `Red: 0, Green: 0, Blue: 0`})
-});
-
-app.get('/api/modes_off', cors(), (req, res) => {
-	console.log("modes_off called")
-	lightController.stroboscopic_off();
-	lightController.rainbow_off();
-	res.send({message: "Modes turned off"})
-});
-
-
-app.get('/api/setColor', cors(), (req, res) => {
-	const {r, g, b} = req.query;
-
-	if(r || g || b){
-		if((r>255 || g>255 || b>255) || (r<0 || g<0 || b<0))
-		{
-			res.status(500);
-			res.send({message: "All the parameters must be numbers within 0 - 255"})
-			return
-		}
-		lightController.set_color(r, g, b);
-		res.send({
-			error: false,
-			data: {message: `Colors: Red: ${r}, Green: ${g}, Blue: ${b}`}
-		});
-		return;
-	}
-	
-	res.status(500);
-	res.send({message: `Invalid colors: Red: ${r}, Green: ${g}, Blue: ${b}`})
-});
-
-app.get('/api/toggleStroboscopic', cors(), (req, res) => {
-	lightController.rainbow_off();
-	const { red, green, blue } = lightController.state.currentColor;
-	if(lightController.state.stroboscop)
-	{
-		lightController.stroboscopic_off();
-		res.send({message: "Stroboscopic-off"})
-		lightController.set_color(red, green, blue);
-	}
-	else
-		if((red || green || blue) && lightController.state.rainbow==false)
-		{
-			lightController.stroboscopic_on();
-			res.send({message: "Stroboscopic-on"})
-		}	
-		else
-		{
-			res.status(500);
-			res.send({message: "Please select colors or close other modes before performing this action!"})
-		}
-});
-
-app.get('/api/toggleRainbow', cors(), (req, res) => {
-	lightController.stroboscopic_off();
-	if(lightController.state.rainbow)
-	{
-		lightController.rainbow_off();
-		res.send({message: "Rainbow_off"})
-	}
-	else
-	{
-		if(lightController.state.stroboscop==false)
-		{
-			lightController.rainbow_on();
-			res.send({message: "Rainbow-on"})
-			console.log("toggleRainbow");
-		}
-		else
-			res.send({message: "Please close other modes before performing this action!"})
-	}
-});
-
-app.get('/api/returnState', cors(), (req, res) => {
+function state()
+{
 	const { red, green, blue } = lightController.state.currentColor;
 	let currentMode = "mode";
-
 	if(lightController.state.stroboscop && lightController.state.rainbow == false)
 		currentMode = "strobo";
 	if(lightController.state.stroboscop == false && lightController.state.rainbow)
@@ -106,6 +24,93 @@ app.get('/api/returnState', cors(), (req, res) => {
 			currentMode: currentMode
 		}
 	})
+}
+
+app.use('/', express.static('public'));
+
+app.get('/api/lights_off', cors(), (req, res) => {
+	console.log("lights_off called")
+	lightController.stroboscopic_off();
+	lightController.rainbow_off();
+	lightController.lights_off();
+	res.send({message: "Lights turned off", color: `Red: 0, Green: 0, Blue: 0`}, state())
+});
+
+app.get('/api/modes_off', cors(), (req, res) => {
+	console.log("modes_off called")
+	lightController.stroboscopic_off();
+	lightController.rainbow_off();
+	res.send({message: "Modes turned off"}, state())
+});
+
+
+app.get('/api/setColor', cors(), (req, res) => {
+	const {r, g, b} = req.query;
+
+	if(r || g || b){
+		if((r>255 || g>255 || b>255) || (r<0 || g<0 || b<0))
+		{
+			res.status(500);
+			res.send({message: "All the parameters must be numbers within 0 - 255"}, state())
+			return
+		}
+		lightController.set_color(r, g, b);
+		res.send({
+			error: false,
+			data: {message: `Colors: Red: ${r}, Green: ${g}, Blue: ${b}`},
+			state()
+		});
+		return;
+	}
+	
+	res.status(500);
+	res.send({message: `Invalid colors: Red: ${r}, Green: ${g}, Blue: ${b}`}, state())
+});
+
+app.get('/api/toggleStroboscopic', cors(), (req, res) => {
+	lightController.rainbow_off();
+	const { red, green, blue } = lightController.state.currentColor;
+	if(lightController.state.stroboscop)
+	{
+		lightController.stroboscopic_off();
+		res.send({message: "Stroboscopic-off"}, state())
+		lightController.set_color(red, green, blue);
+	}
+	else
+		if((red || green || blue) && lightController.state.rainbow==false)
+		{
+			lightController.stroboscopic_on();
+			res.send({message: "Stroboscopic-on"}, state())
+		}	
+		else
+		{
+			res.status(500);
+			res.send({message: "Please select colors or close other modes before performing this action!"}, state())
+		}
+});
+
+app.get('/api/toggleRainbow', cors(), (req, res) => {
+	lightController.stroboscopic_off();
+	if(lightController.state.rainbow)
+	{
+		lightController.rainbow_off();
+		res.send({message: "Rainbow_off"}, state())
+	}
+	else
+	{
+		if(lightController.state.stroboscop==false)
+		{
+			lightController.rainbow_on();
+			res.send({message: "Rainbow-on"}, state())
+			console.log("toggleRainbow");
+		}
+		else
+			res.send({message: "Please close other modes before performing this action!"} , state())
+	}
+});
+
+app.get('/api/returnState', cors(), (req, res) => {
+	state()
 });
 
 
