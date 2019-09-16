6913cd99ed723b6c3304f9f179ad69884d8f7b25
Sergiu Micorici <sergiumicorici@gmail.com>
2019-09-16 16:18:12 +0300

    test nodemon 2

--- index.js
+++ index.js
@@ -146,7 +146,6 @@ app.get('/api/returnState', cors(), (req, res) => {
 
 app.listen(3000, () => {
 	console.log("Listening on port 3000");
-	console.log("new message");
 })
 
 // io.on('connection', function (socket) {
