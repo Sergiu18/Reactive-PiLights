5a16f5e4b1e89557e2e5eebac141c6d041ad5e0e
Sergiu Micorici <sergiumicorici@gmail.com>
2019-09-19 18:08:38 +0300

    try 2

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/reactApp/build'));
+app.use(function(request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 function getState()
 {
