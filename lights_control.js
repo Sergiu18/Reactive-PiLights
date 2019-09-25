f6cd65d7f84d549b314b0c3d0d6cf1fa96aacac9
Sergiu Micorici <sergiumicorici@gmail.com>
2019-09-25 18:34:34 +0300

    testing

--- lights_control.js
+++ lights_control.js
@@ -119,7 +119,7 @@ function breathing_on(emitStateChange)
 	rainbowColorAux = state.currentColor;
 	var timeouts = [];
 	breathingLoop = setInterval(() => {
-		for (let i = 0; i <= 60; ++i)
+		for (let i = 0; i <= 65; ++i)
 		{	
 			timeouts.push(setTimeout(function(){
 				if(state.breathing==true)
